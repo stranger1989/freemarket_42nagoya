75efffbfaa24f1aadcefa316b555f017ecf94751
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-13 13:53:13 +0900

    Create_User_Schema

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190113033510) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                             default: "", null: false
+    t.text     "profile",                limit: 65535
+    t.text     "avatar",                 limit: 65535
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "lastname",                             default: "", null: false
+    t.string   "firstname",                            default: "", null: false
+    t.string   "lastname_kana",                        default: "", null: false
+    t.string   "firstname_kana",                       default: "", null: false
+    t.string   "postalcode",                           default: "", null: false
+    t.string   "prefecture",                           default: "", null: false
+    t.string   "city",                                 default: "", null: false
+    t.string   "block"
+    t.string   "building"
+    t.date     "birthday",                                          null: false
+    t.string   "phone_number",                         default: "", null: false
+    t.string   "payment",                              default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+end
