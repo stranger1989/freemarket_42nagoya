59fcf129eca9cbed04dc97a79cec555e747bad39
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-13 19:40:05 +0900

    Add_Function_For_jp.prefecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_id).try(:name)
+  end
+
+  def prefecture_name=(prefecture)
+    self.prefecture_id = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
