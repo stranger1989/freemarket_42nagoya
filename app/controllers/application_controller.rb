2a13cd288805e133322229cdb96cd5afefc8d278
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-14 16:23:02 +0900

    Solve_TooLong_Sentence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,24 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :avatar, :lastname, :firstname, :lastname_kana, :firstname_kana, :postalcode, :prefecture, :city, :block, :building, :birthday, :phone_number, :payment])
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [:nickname,
+             :profile,
+             :avatar,
+             :lastname,
+             :firstname,
+             :lastname_kana,
+             :firstname_kana,
+             :postalcode,
+             :prefecture,
+             :city,
+             :block,
+             :building,
+             :birthday,
+             :phone_number,
+             :payment])
   end
 
   def production?
