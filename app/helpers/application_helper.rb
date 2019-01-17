840d13974dfac349f6892a2eb24f7837980e36bf
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-17 16:58:43 +0900

    Create_Devise_ErrorFunction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def devise_error_messages(user_param_name)
+    return "" if resource.errors.empty?
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    messages = resource.errors.messages[user_param_name].each do |msg|
+      html += <<-EOF
+        <div class="error_field alert alert-danger" role="alert">
+          <p class="error_msg">#{msg}</p>
+        </div>
+      EOF
+    end
+    html.html_safe
+  end
+
 end
