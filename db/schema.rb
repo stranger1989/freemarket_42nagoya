5f5aab903035897094b3c2643d7f0409b3d3fefe
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-20 18:30:36 +0900

    create category_brand table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190120080744) do
+ActiveRecord::Schema.define(version: 20190120090049) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -21,11 +21,20 @@ ActiveRecord::Schema.define(version: 20190120080744) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
     t.index ["name"], name: "index_categories_on_name", using: :btree
+  end
+
+  create_table "category_brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "brand_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["brand_id"], name: "index_category_brands_on_brand_id", using: :btree
+    t.index ["category_id"], name: "index_category_brands_on_category_id", using: :btree
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -74,5 +83,7 @@ ActiveRecord::Schema.define(version: 20190120080744) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "category_brands", "brands"
+  add_foreign_key "category_brands", "categories"
   add_foreign_key "items", "users"
 end
