de97c93aca2242856d3ddacdb53ee9b0c18a3f9b
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-20 17:17:21 +0900

    add to categories table ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190118100644) do
+ActiveRecord::Schema.define(version: 20190120080744) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 20190118100644) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
-    t.integer  "parent_id",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
     t.index ["name"], name: "index_categories_on_name", using: :btree
   end
 
