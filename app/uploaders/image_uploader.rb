978da6786241c3edce24f3a30b44eab18cf76a5e
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-20 14:44:42 +0900

    Modified image storage settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -30,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_fit: [150, 150]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
