a4430c5544c79640d7f6e76238b6b1340f690fcd
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-23 19:22:18 +0900

    Delete_Original_Method_Registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -109,41 +109,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  def update_needs_confirmation?(resource, previous)
-    resource.respond_to?(:pending_reconfirmation?) &&
-      resource.pending_reconfirmation? &&
-      previous != resource.unconfirmed_email
-  end
-
-  def update_resource(resource, params)
-    resource.update_with_password(params)
-  end
-
-  def build_resource(hash = {})
-    self.resource = resource_class.new_with_session(hash, session)
-  end
-
-  def sign_up(resource_name, resource)
-    sign_in(resource_name, resource)
-  end
-
-  def after_sign_up_path_for(resource)
-    after_sign_in_path_for(resource) if is_navigational_format?
-  end
-
-  def after_inactive_sign_up_path_for(resource)
-    scope = Devise::Mapping.find_scope!(resource)
-    router_name = Devise.mappings[scope].router_name
-    context = router_name ? send(router_name) : self
-    context.respond_to?(:root_path) ? context.root_path : "/"
-  end
-
-  def sign_up_params
-    devise_parameter_sanitizer.sanitize(:sign_up)
-  end
-
-  def translation_scope
-    'devise.registrations'
-  end
-
 end
