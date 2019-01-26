5a6befc94452c5fb9a100daeb4bb3569e270eeba
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-26 21:01:43 +0900

    Add category_id to item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,14 +15,16 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+
   end
-  
+
   def show
   end
-  
+
   private
   def item_params
-    params.require(:item).permit(:name, :image, :price, :order_status, :item_status, :shipping_fee, :delivery_way, :shipping_area, :estimated_shipping_date, :description, :size).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :price, :order_status, :item_status, :shipping_fee, :delivery_way, :shipping_area, :estimated_shipping_date, :description, :size, :category_id).merge(user_id: current_user.id)
   end
 
 end
+
