4f5dbf597f309af0c8579b22438c064d5b346e0c
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-26 14:58:09 +0900

    Revise_BeforeAction_ItemAndOrderController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def index
+    @items = Item.where("order_status = '出品中'")
   end
 
   def new
@@ -20,19 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @items = Item.where("order_status = '出品中'")
+    @item = Item.find(params[:id])
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :image, :price, :order_status, :item_status, :shipping_fee, :delivery_way, :shipping_area, :estimated_shipping_date, :description, :size).merge(user_id: current_user.id)
-  end
-
-  def set_all_items
-    @items = Item.where("order_status = '出品中'")
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
   end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:new, :create]
 
   def new
