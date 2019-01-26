2e5f914b28b4a7b82fbf5aef83fb9b39bb5d8acb
kuroda-kentaro <kuroda_business@outlook.jp>
2019-01-26 12:55:35 +0900

    Add_setItems_To_ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-
+  before_action :set_all_items, only: [:index, :show]
+  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, only: [:create]
 
   def index
   end
@@ -16,13 +18,21 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :price, :order_status, :item_status, :shipping_fee, :delivery_way, :shipping_area, :estimated_shipping_date, :description, :size).merge(user_id: current_user.id)
+  end
+
+  def set_all_items
+    @items = Item.where("order_status = '出品中'")
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
