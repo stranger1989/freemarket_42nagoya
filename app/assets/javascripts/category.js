91f289000d5e5164d9f43585424104644ee18542
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-26 21:43:06 +0900

    modified indent

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -5,9 +5,9 @@ $(document).on('turbolinks:load', function() {
 // 大カテゴリが選択された時
   $('.root_category').change(function(event, template) {
     var val = $('.root_category').val();
-      var template = $(`#child_of${val}`);
-      $(this).nextAll().remove();
-      $(this).parent().append(template.html());
+    var template = $(`#child_of${val}`);
+    $(this).nextAll().remove();
+    $(this).parent().append(template.html());
   });
 
 // 中カテゴリが選択された時
