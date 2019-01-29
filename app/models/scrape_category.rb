59871d467f81961c5dfc90563d968f9def4be4b8
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-01-29 15:16:04 +0900

    Modified user-agent

--- app/models/scrape_category.rb
+++ app/models/scrape_category.rb
@@ -13,10 +13,9 @@ url = "https://www.mercari.com/jp/category/"
 charset = nil
 
 # User-Agentの設定
-# opt = {}
-# opt['User-Agent'] = 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36'
+user_agent = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36"
 
-html = open(url, "user-agent" => mrcr42) do |page|
+html = open(url, "User-Agent" => user_agent) do |page|
   #charsetを取得
   charset = page.charset
   #htmlの読み込み
