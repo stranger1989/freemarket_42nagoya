bbca649ce180914369c4a9f2816ef5964e96fe5c
Ryosuke Komaki <kmkkiikkmk@gmail.com>
2019-02-03 19:36:06 +0900

    Add edit/update/delete action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :update, :destroy]
 
   def index
     @items = Item.where("order_status = '出品中'")
@@ -16,17 +17,39 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-
   end
 
   def show
     @items = Item.where("order_status = '出品中'")
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:item_id])
+    render "items/#{params[:name]}"
+  end
+
+  def update
+    if @item.user.id == current_user.id
+      if @item.update(item_params)
+        render "items/myitem-detail"
+      else
+        render "items/edit"
+      end
+    end
+  end
+
+  def destroy
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to action: :index
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :image, :price, :order_status, :item_status, :shipping_fee, :delivery_way, :shipping_area, :estimated_shipping_date, :description, :size, :category_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
