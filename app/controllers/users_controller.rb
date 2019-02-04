4acc909018097c02d43f1a44e8f1e16fe6c586fc
kuroda-kentaro <kuroda_business@outlook.jp>
2019-02-04 11:57:07 +0900

    Set_SearchVal_For_UserMypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_search_val, only: [:show]
   def show
+  end
+
+  private
+  def set_search_val
+    @q = Item.ransack(params[:q])
+    @categories = Category.all
+    @items = @q.result.includes(:category, :brand).page(params[:page]).per(48)
+    @count = @items.total_count
   end
 end
